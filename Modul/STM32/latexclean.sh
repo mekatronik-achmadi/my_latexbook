ffca54ad5d4f09801e15e037f1441bb5f24e88ce
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-09-05 22:35:09 +0700

    stm32 ppt

---
+++ Modul/STM32/latexclean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -vf *.pdf
+rm -vf *.log
+rm -vf *.toc
+rm -vf *.synctex.gz
+rm -vf *.aux
+rm -vf *.out
+rm -vf *.bbl
+rm -vf *.blg
+rm -vf *.lof
+rm -vf *.nav
+rm -vf *.snm
+rm -rvf ./_minted*
