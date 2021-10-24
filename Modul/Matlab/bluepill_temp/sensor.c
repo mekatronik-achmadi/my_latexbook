fe5523c0e695ae3ca0ee16ba48f3a145e3cf72b7
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-10-24 22:21:16 +0700

    I2C are trash

--- Modul/Matlab/bluepill_temp/sensor.c
+++ Modul/Matlab/bluepill_temp/sensor.c
@@ -3,12 +3,15 @@
 #include "gfx.h"
 
 #include "chprintf.h"
+#include "i2c.h"
 #include "i2c_lld.h"
 #include "pal_lld.h"
 #include "stdint.h"
 
 #include "data.h"
 #include "sensor.h"
+
+#define I2CPORT I2CD2
 
 #define addr_sensor 0x40
 
@@ -17,24 +20,24 @@ static uint8_t sensor_rx_data[8];
 static uint8_t sensor_tx_data[8];
 
 void HTU21DStart(void){
-  systime_t tmo = MS2ST(4);
+  systime_t tmo = MS2ST(10);
 
   sensor_tx_data[0] = 0xFF;
   
   /* sending */
-  i2cAcquireBus(&I2CD1);
-  i2cMasterTransmitTimeout(&I2CD1, addr_sensor,sensor_tx_data, 1, sensor_rx_data, 0, tmo);
-  i2cReleaseBus(&I2CD1);
+  i2cAcquireBus(&I2CPORT);
+  i2cMasterTransmitTimeout(&I2CPORT, addr_sensor,sensor_tx_data, 1, sensor_rx_data, 0, tmo);
+  i2cReleaseBus(&I2CPORT);
 }
 
 uint16_t HTU21DGetVal(void){
-  systime_t tmo = MS2ST(4);
+  systime_t tmo = MS2ST(10);
 
   sensor_tx_data[0] = 0xE3;
 
-  i2cAcquireBus(&I2CD1);
-  i2cMasterTransmitTimeout(&I2CD1, addr_sensor,sensor_tx_data, 1, sensor_rx_data, 2, tmo);
-  i2cReleaseBus(&I2CD1);
+  i2cAcquireBus(&I2CPORT);
+  i2cMasterTransmitTimeout(&I2CPORT, addr_sensor,sensor_tx_data, 1, sensor_rx_data, 2, tmo);
+  i2cReleaseBus(&I2CPORT);
 
   return (sensor_rx_data[0]<<8) | sensor_rx_data[1];
 }
@@ -45,9 +48,9 @@ uint8_t HTU21DTest(void){
 
   sensor_tx_data[0] = 0xE3;
 
-  i2cAcquireBus(&I2CD1);
-  status = i2cMasterTransmitTimeout(&I2CD1, addr_sensor,sensor_tx_data, 1, sensor_rx_data, 2, tmo);
-  i2cReleaseBus(&I2CD1);
+  i2cAcquireBus(&I2CPORT);
+  status = i2cMasterTransmitTimeout(&I2CPORT, addr_sensor,sensor_tx_data, 1, sensor_rx_data, 2, tmo);
+  i2cReleaseBus(&I2CPORT);
 
   return status;
 }
@@ -64,8 +67,12 @@ static const I2CConfig i2cfg = {
 };
 
 void sensor_init(void){
-  palSetPadMode(GPIOB,6,PAL_MODE_STM32_ALTERNATE_OPENDRAIN);
-  palSetPadMode(GPIOB,7,PAL_MODE_STM32_ALTERNATE_OPENDRAIN);
-  i2cStart(&I2CD1, &i2cfg);
+
+  palSetPadMode(GPIOB,10,PAL_MODE_STM32_ALTERNATE_OPENDRAIN);
+  palSetPadMode(GPIOB,11,PAL_MODE_STM32_ALTERNATE_OPENDRAIN);
+  i2cStart(&I2CPORT, &i2cfg);
+
+  chThdSleepMilliseconds(10);
+  
   HTU21DStart();
 }
